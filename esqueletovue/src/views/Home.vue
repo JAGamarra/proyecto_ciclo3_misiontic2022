df053a8706f53eaf84aaf889251734bce6170915
jcbetanm <jcbetanm@gmail.com>
2021-09-23 18:55:55 -0500

    esqueleto vue

---
+++ esqueletovue/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template >
+  
+  <v-container fluid class="transparent">
+        <h1>Home</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quae ex minus debitis dolorum, incidunt pariatur, placeat voluptatem, ipsam praesentium blanditiis earum nesciunt quo laborum repellat sint alias deserunt voluptates.</p>
+  </v-container>
+</template>
+
+<script>
+ 
+
+  export default {
+    name: 'Home',
+
+    components: {
+   
+    },
+  }
+</script>
+
+
