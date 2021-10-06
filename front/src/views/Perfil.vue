8d683ae78fefc31c510c1ab2bc39c84e05ec6b33
jcamilobm <jcbetanm@gmail.com>
2021-10-06 09:05:56 -0500

    dependencia para sibir archivos

--- front/src/views/Perfil.vue
+++ front/src/views/Perfil.vue
@@ -1,78 +1,94 @@
 <template>
-  <!-- <div>
-     <v-btn @click="getIDUser()">
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-  </div> -->
+ 
 
-  <v-container fluid>
-    <!-- // imagen de perfil ,nombre usuario -->
-    <v-row>
-      <v-col cols="12" lg="12">
-        <v-card class="mx-auto" max-width="434" tile>
-          <v-img
-            height="230px"
-            src="https://img.pixers.pics/pho(s3:700/PI/12/81/53/36/6/700_PI128153366_cfe7f3c0baa299e38f843df4ef584ad3_5b7abea34e466_.,700,462,jpg)/cortinas-transparentes-mosaico-azul-poligonal.jpg.jpg"
-          >
-            <v-row align="end" class="fill-height">
-              <v-col align-self="start" class="pa-0" cols="12">
-                <v-avatar class="profile" color="grey" size="164" tile>
-                  <v-img
-                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                  ></v-img>
-                </v-avatar>
-              </v-col>
-              <v-col class="py-0">
-                <v-list-item color="rgba(0, 0, 0, .4)" dark>
-                  <v-list-item-content>
-                    <v-list-item-title class="text-h6">
-                      {{name}}  {{lastname}}
-                    </v-list-item-title>
-                    <v-list-item-subtitle
-                      >{{userType}}</v-list-item-subtitle
-                    >
-                  </v-list-item-content>
-                </v-list-item>
-              </v-col>
-            </v-row>
-          </v-img>
+  <v-card>
+    <v-toolbar
+      flat
+      color="primary"
+      dark
+    >
+      <v-toolbar-title>User Profile</v-toolbar-title>
+    </v-toolbar>
+    <v-tabs vertical>
+      <v-tab>
+        <v-icon left>
+          mdi-account
+        </v-icon>
+        Option 1
+      </v-tab>
+      <v-tab>
+        <v-icon left>
+          mdi-lock
+        </v-icon>
+        Option 2
+      </v-tab>
+      <v-tab>
+        <v-icon left>
+          mdi-access-point
+        </v-icon>
+        Option 3
+      </v-tab>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
+            </p>
+
+            <p>
+              Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit amet velit hendrerit rutrum.
+            </p>
+
+            <p class="mb-0">
+              Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
+            </p>
+          </v-card-text>
         </v-card>
-      </v-col>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nunc sed turpis.
+            </p>
 
-      <v-col cols="12" lg=12>
-        <v-card class=" mx-auto d-flex  flex-wrap" max-width="230" elevation=4>
+            <p>
+              Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra condimentum.
+            </p>
 
-          <v-card-subtitle class="pb-0"> username</v-card-subtitle>
-          <v-card-text class="text--primary pb-0">
-                {{username}}
+            <p>
+              Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
+            </p>
+
+            <p>
+              Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce fermentum odio nec arcu.
+            </p>
+
+            <p class="mb-0">
+              Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
+            </p>
           </v-card-text>
-
-          <v-card-subtitle class="pb-0"> password </v-card-subtitle>
-          <v-card-text class="text--primary pb-0">
-            {{password}}
-          </v-card-text>
-
-          
-          <v-card-subtitle class="pb-0"> correo </v-card-subtitle>
-          <v-card-text class="text--primary pb-0">
-            {{email}}
-          </v-card-text>
-
-           <v-card-subtitle class="pb-0"> cellphone </v-card-subtitle>
-          <v-card-text class="text--primary pb-0">
-            {{cellphone}}
-          </v-card-text>
-
         </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
+            </p>
 
-      </v-col>
+            <p class="mb-0">
+              Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
 
 
 
-
-
-    </v-row>
-  </v-container>
 </template>
 
 <script>
