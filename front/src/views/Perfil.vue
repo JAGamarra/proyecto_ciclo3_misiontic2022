b1fd57f69cd02acbf5eff72b03242c0a85b13cc2
jarangoz11 <jarangoz.spain@gmail.com>
2021-10-11 21:54:04 -0500

    Prototipo

--- front/src/views/Perfil.vue
+++ front/src/views/Perfil.vue
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <template>
   <v-card>
     <v-toolbar flat color="primary" dark>
@@ -242,4 +240,3 @@ export default {
 </script>
 
 <style></style>
->>>>>>> 1c8f94259fe7cd8590f739330cb64a7c5ab24d62
